6ec8aa87de0109974528c4d96b0da6f4963f2b51
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-10 19:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeElement(){
+	let options = document.getElementById("colorSelect")
+	let value = options.options[options.selectedIndex].value
+	let element = document.getElementById(value)
+	element.remove()
+}
